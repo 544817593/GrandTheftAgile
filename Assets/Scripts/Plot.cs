bacd2c8873e86bf91fec9d1f025e33c7ddadc96d
Ruichen Gao <32527764+544817593@users.noreply.github.com>
2025-03-04 10:10:21 +0000

    Shop

--- Assets/Scripts/Plot.cs
+++ Assets/Scripts/Plot.cs
@@ -11,7 +11,16 @@ public class Plot : MonoBehaviour
     {
         if (turret != null) return;
 
-        GameObject turretToBuild = BuildManager.instance.GetSelectedTurret();
-        turret = Instantiate(turretToBuild, transform.position, Quaternion.identity);
+        Tower turretToBuild = BuildManager.instance.GetSelectedTurret();
+
+        if (turretToBuild.cost > LevelManager.Instance.currency)
+        {
+            Debug.Log("Not enough currency to build that!");
+            return;
+        }
+
+        LevelManager.Instance.DecreaseCurrency(turretToBuild.cost);
+
+        turret = Instantiate(turretToBuild.prefab, transform.position, Quaternion.identity);
     }
 }
